c691993bbe77fcb8c1487a76c3e010718a93c1e8
Jayanna G <119968609+JayaG-gowda@users.noreply.github.com>
2025-03-05 15:36:54 +0530

    Update Hostel_Room_Allocation_System.cpp

--- C++ projects/Hostel_Room_Allocation_System/Hostel_Room_Allocation_System.cpp
+++ C++ projects/Hostel_Room_Allocation_System/Hostel_Room_Allocation_System.cpp
@@ -3,20 +3,20 @@
 #include<iostream.h>
 #include<conio.h>
 #include<stdlib.h>
-//#define n 5   //this is for constant rooms
+//#define n 5
 
-int i;
+int i,k=0;
 void display_name(char ch[100][10], int nu)	//displaying rooms with occupied persons
 {
  cout<<"The allocated rooms details\n";
- cout<<"  Rooms     Member\n";
+ cout<<"  Rooms    Students\n";
  for(i=0;i<nu;i++)
  {
   cout<<i+1<<". Room"<<i+1<<" = "<<ch[i]<<endl;
  }
 }
 
-void display_room(int r[], int nu)		//displaying available rooms only (not already alloted)
+int display_room(int r[], int nu)		//displaying available rooms only (not already alloted)
 {
  cout<<"The avilable rooms are\n";
  for(i=0;i<nu;i++)
@@ -25,12 +25,22 @@ void display_room(int r[], int nu)		//displaying available rooms only (not alrea
   {
    cout<<i+1<<". Room"<<i+1<<endl;
   }
+  /*else
+  {
+   cout<<"All rooms are occupied.\n";
+   k+= 1;
+   break;
+  } */
  }
+ /*if(k==nu)
+  return 1;
+ else
+  return 0;*/
 }
 
 void main()
 {
- int rn, op, n, R[100]={NULL};	//rn means entered choice of room number, R[] holds the rooms number (also it holds, wheather given room is alloted or not)
+ int rn, op, n, x=0, R[100]={NULL};	//rn means entered choice of room number, R[] holds the rooms number (also it holds, wheather given room is alloted or not)
  char name[100][10]={NULL};	//name[] holds the occupied persons names
  clrscr();
 
@@ -49,30 +59,33 @@ void main()
   switch(op)
   {
    case 1:		//here we check and allocate the room to the customer
-	  display_room(R,n);
-	  cout<<"Enter the available room number to allocate\n";
-	  cin>>rn;
-
-	  if(rn<=n && rn>=0)		//checking wheather given room number is there in the present room list
+	  x = display_room(R,n);
+	  if(x!=1)
 	  {
-	   if(R[rn-1]==0)	//check the room is empty or not
+	   cout<<"Enter the available room number to allocate\n";
+	   cin>>rn;
+
+	   if(rn<=n && rn>=0)		//checking wheather given room number is there in the present room list
 	   {
-	    cout<<"Room is available...!!\nPlease enter your name to allocate room with a conditions.\n1. Space is not allowed (Jayanna_G).\n2. letter should be less than 10 character\n";
-	    cin>>name[rn-1];
-	    R[rn-1]=1;
-	    cout<<"Congratulations room"<<rn<<" is allocated to "<<name[rn-1]<<endl;
-	    break;
+	    if(R[rn-1]==0)	//check the room is empty or not
+	    {
+	     cout<<"Room is available...!!\nPlease enter your name to allocate room with a conditions.\n1. Space is not allowed (Jayanna_G).\n2. letter should be less than 10 character\n";
+	     cin>>name[rn-1];
+	     R[rn-1]=1;
+	     cout<<"Congratulations room"<<rn<<" is allocated to "<<name[rn-1]<<endl;
+	     break;
+	    }
+	    else
+	    {
+	     cout<<"Room"<<rn<<" is already occupied by "<<name[rn-1]<<endl;
+	     break;
+	    }
 	   }
 	   else
 	   {
-	    cout<<"Room"<<rn<<" is already occupied by "<<name[rn-1]<<endl;
+	    cout<<"Enter the valid room number\n";
 	    break;
 	   }
-	  }
-	  else
-	  {
-	   cout<<"Enter the valid room number\n";
-	   break;
 	  }
 	  break;
 
